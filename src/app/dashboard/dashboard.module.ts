4bd9259b80640bcfbd7bc53d82cc97f0d565210c
Javier Nicolas Fernandez <nicofernandez97@hotmail.com>
2023-10-14 15:57:40 -0300

    Primer commit

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    MatSidenavModule
+  ],
+  exports:[DashboardComponent]
+})
+export class DashboardModule { }
